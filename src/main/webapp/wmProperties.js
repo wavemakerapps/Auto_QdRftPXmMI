452527a050f8516fa4c282137a62c99fb6a9d567
root <root@54620d7d764c>
2017-06-16 14:27:31 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_QdRftPXmMI",
+  "homePage" : "Main",
+  "name" : "Auto_QdRftPXmMI",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
